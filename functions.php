5a6969fc9dec8c314067c2b88f5e978df804cfb0
Camille Tromp <camille.tromp@gmail.com>
2023-11-03 23:16:58 +0100

    mise en place thème personnalisé, fonts, css...

--- functions.php
+++ functions.php
@@ -4,7 +4,23 @@
 **/
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
- wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
- wp_enqueue_style( 'theme-style', get_stylesheet_directory_uri() . '/assets/css/stylechild.css', array(), time() );
+ wp_enqueue_style( 'theme-style', get_stylesheet_directory_uri() . '/assets/css/motaphoto.css', array(), time() );
 
 }
+
+/**page administration du thème */
+function motaphoto_add_admin_pages(){
+    add_menu_page(__('Paramètres du thème MotaPhoto', 'motaphoto'), __('MotaPhoto', 'motaphoto'), 'manage_options', 'motaphoto-settings', 'motaphoto_theme_settings', 'dashicons-admin-settings', 60);
+}
+
+function motaphoto_theme_settings() {
+    echo '<h1>' .get_admin_page_title(). '</h1>';
+}
+
+add_action('admin_menu', 'motaphoto_add_admin_pages', 10);
+
+/** menu principal */
+function register_my_menu() {
+    register_nav_menu('main-menu', 'Menu principal');
+}
+add_action( 'after_setup_theme', 'register_my_menu' );
---
+++ header.php
@@ -0,0 +1,22 @@
+
+<!doctype html>
+<html lang="fr">
+<head>
+	<meta charset="uft-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<title>MotaPhoto</title>
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&family=Space+Mono:wght@400;700&display=swap" rel="stylesheet">
+	<?php wp_head(); ?>
+</head>
+
+<body>
+
+<?php /*affiche mon menu */
+ wp_nav_menu([
+    'theme_location' => 'main-menu',
+]); 
+?>
+
+
